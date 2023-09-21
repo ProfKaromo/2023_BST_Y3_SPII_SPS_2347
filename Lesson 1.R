64c9e215c20cd6e00ba205c36b13123914ca2f8f
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2023-09-21 09:44:30 +0300

    Uloaded Lesson 2

--- Lesson 1.R
+++ Lesson 1.R
@@ -1,3 +1,4 @@
+cat("Lesson One: Version Controls in Rstudio")
 print("Hello world")
 cat("Hello World")
 print("I love programming")
---
+++ Lesson 2.R
@@ -0,0 +1,6 @@
+#DEALING WITH MATRICES
+# A matrix of order 5*5 whose elements are random normal 
+# values with mean 6 and s.d of 2
+
+# Solution
+
