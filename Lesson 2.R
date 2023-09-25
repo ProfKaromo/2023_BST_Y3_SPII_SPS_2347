a5a300ff2b92eb2936a4bc0022a7ce685949af61
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2023-09-25 13:01:20 +0300

    Updated the Lesson 2 and the uploded the Class annoucements and the Class Marks file.

--- Lesson 2.R
+++ Lesson 2.R
@@ -3,4 +3,24 @@
 # values with mean 6 and s.d of 2
 
 # Solution
+A <- matrix(round(rnorm(25,6,2),0),nrow = 5, byrow = T)
+A
 
+# Matrix Operations
+X <- matrix(c(1:9), nrow = 3, byrow = T)
+Y <- matrix(c(1:9), nrow = 3, byrow = F)
+X;Y
+
+X+Y
+A+X#not posible
+
+#Scalar with a matrix
+3*X
+
+Y-X
+
+# Extracting of values
+
+Y[2,3]
+Y[3,]
+Y[,2]
