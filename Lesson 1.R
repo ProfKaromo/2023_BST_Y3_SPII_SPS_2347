84e628cf2d0a31ce5144286dfc7aa1b7a80389ca
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2023-10-02 11:43:54 +0300

    Updated Codes

--- Lesson 1.R
+++ Lesson 1.R
@@ -2,3 +2,24 @@ cat("Lesson One: Version Controls in Rstudio")
 print("Hello world")
 cat("Hello World")
 print("I love programming")
+
+
+mysample <- c(1:10)
+xbar <- c(102,94.8,98.3,98.4,102,98.5,99,97.7,100,98.1)
+mydata <- as.data.frame(cbind(mysample,xbar))
+mydata
+xbar_mu0 <- xbar-99
+xbar_mu0
+mydata <- cbind(mydata,xbar_mu0)
+mydata
+
+s <- NULL
+mu0 <- 99
+s[1] <- xbar[1]-mu0
+for (i in 2:length(mysample)) {
+  s[i] <- (xbar[i]-mu0)+s[i-1]
+}
+s
+
+mydata <- cbind(mydata,s)
+mydata
--- Lesson 2.R
+++ Lesson 2.R
@@ -24,3 +24,65 @@ Y-X
 Y[2,3]
 Y[3,]
 Y[,2]
+
+Z <- X%*%Y
+Z
+
+tX <- t(X)
+tX
+X
+
+X[2,1] <- 5
+  
+solve(X)
+
+#2X+Y-2XY
+Y[2,1] <- 0
+K <- 2*X+t(Y)-2*X%*%solve(Y)
+K
+
+x1 <- 2*X
+x2 <- t(Y)
+x3 <- 2*X%*%solve(Y)
+
+K1<- x1+x2-x3
+K1
+
+X;Y
+kronecker(X,Y)
+
+#Identity matrix
+diag(1,5)
+
+X
+diag(diag(X))
+
+X;Y
+
+X[,2]%*%Y
+(as.matrix(X[,2]))%*%Y
+
+X1 <- matrix(round(rnorm(36,3,1),0),nrow=6, byrow=T)
+X1
+
+X1_row_mean <- rowMeans(X1)
+X1_row_mean
+X1_col_mean <- colMeans(X1)
+X1_col_mean
+
+K1 <- as.data.frame(X1)
+k2 <- sapply(K1,K1$V1, max)
+k2
+
+#logs
+log10(5)
+log(5,10)
+log(5,base = 10)
+log(5,base = exp(1))
+log(8,16)
+
+#Exponents
+exp(5)
+plot(exp(-(5:50)))
+
+     
